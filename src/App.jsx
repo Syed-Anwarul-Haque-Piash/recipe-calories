5b91c6c5cdf57fd2cde0c5d26baa997149454bfd
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2024-10-22 16:29:06 +0600

    our recipies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner/Banner"
 import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="max-w-7xl mx-auto">
      <Header></Header>
      <Banner></Banner>
+     <Home></Home>
     </div>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-center mt-4'>
+            <h1 className='font-bold text-4xl'>Our Recipies</h1>
+            <h1 className='text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aliquid beatae laboriosam. Deserunt nobis similique vero! Nobis mollitia deserunt ut?</h1>
+        </div>
+    );
+};
+
+export default Home;
